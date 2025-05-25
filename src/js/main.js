98cfa34a5a5a9ea9e64b0cdb0f4f4ab6bd6e8259
alex_sergeev <sergeevalexey808@gmail.com>
2025-05-26 04:23:03 +0900

    add hero

--- src/js/main.js
+++ src/js/main.js
@@ -36,9 +36,15 @@ document.addEventListener('DOMContentLoaded', () => {
       spaceBetween: 10,
       loop: true,
       speed: 300,
+      autoplay: {
+        delay: 5000,
+      },
       breakpoints: {
         768: {
           spaceBetween: 20,
+          autoplay: {
+            delay: 5000,
+          },
         },
         991: {
           spaceBetween: 20,
@@ -49,6 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
         },
       },
     });
+    sayasatSwiper.autoplay.start();
 
     const sayasatSwiperContainer = document.querySelector('.sayasat-swiper');
     sayasatSwiperContainer.addEventListener('mouseenter', () => {
@@ -64,5 +71,33 @@ document.addEventListener('DOMContentLoaded', () => {
     langBtn.onclick = function () {
       langBtn.classList.toggle('language-active');
     };
+  }
+  if (document.querySelector('[js-tab]')) {
+    const tabs = document.querySelectorAll('[js-tab]');
+
+    tabs.forEach((tab) => {
+      const tabBtns = tab.querySelectorAll('[js-tab-btn]');
+      const tabItems = tab.querySelectorAll('[js-tab-item]');
+      tabBtns.forEach((btn) => {
+        tabClick(btn, tabBtns, tabItems);
+      });
+    });
+
+    function tabClick(item, btns, items) {
+      item.addEventListener('click', function (e) {
+        e.preventDefault();
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute('js-tab-data');
+        let currentTabs = document.querySelectorAll(`[js-tab-data="${tabId}"]`);
+
+        if (!currentBtn.hasAttribute('js-tab-active')) {
+          btns.forEach((item) => item.removeAttribute('js-tab-active'));
+          currentBtn.setAttribute('js-tab-active', '');
+          items.forEach((item) => item.removeAttribute('js-tab-active'));
+
+          currentTabs.forEach((item) => item.setAttribute('js-tab-active', ''));
+        }
+      });
+    }
   }
 });
