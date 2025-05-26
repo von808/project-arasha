f3716a699a619491c0734155efdd4ce244c4d625
alex_sergeev <sergeevalexey808@gmail.com>
2025-05-26 21:46:17 +0900

    main page

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+  if (document.querySelector('.header')) {
+    const main = document.querySelector('.main');
+    const header = document.querySelector('.header');
+    const navBar = document.querySelector('.nav-bar-wrapper');
+    function navBarFixed(header, navBar) {
+      let headerH = header.clientHeight;
+      let navBarH = navBar.clientHeight;
+      let windowScroll = Number(window.scrollY.toFixed());
+      // console.log(headerH);
+      // console.log(windowScroll);
+      if (windowScroll > headerH) {
+        navBar.classList.add('fixed');
+        main.style.paddingTop = `${navBarH}px`;
+      } else {
+        navBar.classList.remove('fixed');
+        main.style.paddingTop = null;
+      }
+    }
+    window.addEventListener('scroll', () => {
+      navBarFixed(header, navBar);
+    });
+  }
   if (document.querySelector('.burger-btn')) {
     const burgerBtn = document.querySelector('.burger-btn');
     const burgerClose = document.querySelector('.burger-menu__close');
@@ -29,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
       document.body.classList.toggle('no-scroll');
     }
   }
-  if (document.querySelector('.sayasat-swiper')) {
+  if (document.querySelector('.swiper')) {
     const sayasatSwiper = new Swiper('.sayasat-swiper', {
       a11y: false,
       slidesPerView: 'auto',
@@ -56,13 +78,132 @@ document.addEventListener('DOMContentLoaded', () => {
       },
     });
     sayasatSwiper.autoplay.start();
-
     const sayasatSwiperContainer = document.querySelector('.sayasat-swiper');
     sayasatSwiperContainer.addEventListener('mouseenter', () => {
       sayasatSwiper.autoplay.stop();
     });
     sayasatSwiperContainer.addEventListener('mouseleave', () => {
       sayasatSwiper.autoplay.start();
+    });
+
+    const swiperFoursWrappers = document.querySelectorAll('.swiper-four-wrapper');
+    swiperFoursWrappers.forEach((swiperFoursWrapper) => {
+      const swiper = swiperFoursWrapper.querySelector('.swiper-four');
+      const swiperBtnNext = swiperFoursWrapper.querySelector('.swiper-btn-next');
+      const swiperBtnPrev = swiperFoursWrapper.querySelector('.swiper-btn-prev');
+
+      const swiperFour = new Swiper(swiper, {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: swiperBtnNext,
+          prevEl: swiperBtnPrev,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+        },
+        breakpoints: {
+          576: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          991: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+          },
+        },
+      });
+    });
+
+    const swiperThreeWrappers = document.querySelectorAll('.swiper-three-wrapper');
+    swiperThreeWrappers.forEach((swiperThreeWrapper) => {
+      const swiper = swiperThreeWrapper.querySelector('.swiper-three');
+      const swiperBtnNext = swiperThreeWrapper.querySelector('.swiper-btn-next');
+      const swiperBtnPrev = swiperThreeWrapper.querySelector('.swiper-btn-prev');
+
+      const swiperThree = new Swiper(swiper, {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: swiperBtnNext,
+          prevEl: swiperBtnPrev,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+        },
+        breakpoints: {
+          576: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          991: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+        },
+      });
+    });
+
+    const swiperWorldNewsWrappers = document.querySelectorAll('.world-news-swiper-wrapper');
+    swiperWorldNewsWrappers.forEach((swiperWorldNewsWrapper) => {
+      const swiper = swiperWorldNewsWrapper.querySelector('.world-news-swiper');
+      const swiperBtnNext = swiperWorldNewsWrapper.querySelector('.swiper-btn-next');
+      const swiperBtnPrev = swiperWorldNewsWrapper.querySelector('.swiper-btn-prev');
+
+      const swiperWorldNews = new Swiper(swiper, {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        grid: {
+          rows: 2,
+        },
+        navigation: {
+          nextEl: swiperBtnNext,
+          prevEl: swiperBtnPrev,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+        },
+        breakpoints: {
+          991: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+            grid: {
+              rows: 2,
+            },
+          },
+        },
+      });
+    });
+
+    const swiperDepartmentWrappers = document.querySelectorAll('.department-swiper-wrapper');
+    swiperDepartmentWrappers.forEach((swiperDepartmentWrapper) => {
+      const swiper = swiperDepartmentWrapper.querySelector('.department-swiper');
+      const swiperBtnNext = swiperDepartmentWrapper.querySelector('.swiper-btn-next');
+      const swiperBtnPrev = swiperDepartmentWrapper.querySelector('.swiper-btn-prev');
+
+      const swiperDepartment = new Swiper(swiper, {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        navigation: {
+          nextEl: swiperBtnNext,
+          prevEl: swiperBtnPrev,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+        },
+      });
     });
   }
   if (document.querySelector('.language')) {
